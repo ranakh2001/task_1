3784f0e5bcce0311bb0aea6b35a02e598ab13370
Rana El-khodary <77431301+ranakh2001@users.noreply.github.com>
2024-12-27 14:12:13 +0200

    second task

---
+++ bin/task2.dart
@@ -0,0 +1,33 @@
+class Name {
+  late String firstName;
+  late String surName;
+  bool surnameIsFirst = false;
+
+  Name(String fName, String sName) {
+    firstName = fName;
+    surName = sName;
+  }
+
+  void setSurNameFirst() {
+    surnameIsFirst = true;
+  }
+
+  @override
+  String toString() {
+    if (surnameIsFirst) {
+      print("Your full name is: $surName $firstName ");
+    } else {
+      print("Your full name is: $firstName $surName");
+    }
+
+    return super.toString();
+  }
+}
+
+void main(List<String> args) {
+  Name name1 = Name("Rana", "El-khodary");
+  name1.toString();
+  Name name2 = Name("Nada", "Khalee");
+  name2.setSurNameFirst();
+  name2.toString();
+}
